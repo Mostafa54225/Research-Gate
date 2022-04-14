d95dcdf183b0a61104153efa6ed1cb50dd748244
Mostafa54225 <mostafa.draven@gmail.com>
2022-04-14 02:16:06 +0200

    search controller

--- ResearchGate/Controllers/HomeController.cs
+++ ResearchGate/Controllers/HomeController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using ResearchGate.Models;
 namespace ResearchGate.Controllers
 {
     public class HomeController : Controller
     {
+
+        ResearchGateDBContext db = new ResearchGateDBContext();
         public ActionResult Index()
         {
             return View();
@@ -25,6 +27,45 @@ namespace ResearchGate.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+
+        [HttpGet]
+        
+        public ActionResult search()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult search(string option, string search)
+        {
+
+            var data = (dynamic)null;
+
+            search = search.ToLower();
+
+            switch (option)
+            {
+                case "name":
+                    data = db.Authors.Where(x => x.FirstName.ToLower() == search || x.LastName.ToLower() == search || x.FirstName.ToLower() + 
+                    x.LastName.ToLower() == search || search == null).ToList();
+                    break;
+                case "University":
+                    data = db.Authors.Where(x => x.University.ToLower() == search || search == null).ToList();
+                    break;
+                case "email":
+                    data = db.Authors.Where(x => x.Email.ToLower() == search || search == null).ToList();
+                    break;
+            }
+
+            if(data.Count == 0)
+            {
+                ViewBag.Authors = "Not Found";
+            }
+
+
+            return View("search", data);
         }
     }
 }
